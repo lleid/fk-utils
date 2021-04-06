c7b31681f2236eb96fe314a602000711fa93aec9
lleid <liulei5913@126.com>
2021-04-06 13:34:18 +0800

    update audit test

--- src/test/java/com/fk/framework/audit/AuditUtilsTest.java
+++ src/test/java/com/fk/framework/audit/AuditUtilsTest.java
@@ -1,16 +1,19 @@
 package com.fk.framework.audit;
 
+import com.fk.framework.audit.annotations.AuditModel;
+import com.fk.framework.audit.annotations.AuditProperty;
 import com.fk.framework.audit.beans.AuditVo;
 import com.google.common.collect.Lists;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 
+
 public class AuditUtilsTest {
 
-
     public static void main(String[] args) {
-
         Operator operator1 = new Operator();
         operator1.setName("张三");
         operator1.setAge(10);
@@ -41,3 +44,22 @@ public class AuditUtilsTest {
         System.out.println(auditLog2);
     }
 }
+
+@Getter
+@Setter
+@AuditModel
+class Operator {
+    @AuditProperty(name = "姓名")
+    private String name;
+    @AuditProperty(name = "年纪")
+    private Integer age;
+    @AuditProperty(name = "生日")
+    private Date birthday;
+    @AuditProperty(name = "是否男生")
+    private Boolean isMan = false;
+    @AuditProperty(name = "标签", className = "java.lang.String")
+    private List<String> list;
+    @AuditProperty(name = "下属", className = "com.fk.framework.audit.Operator", include = {"name"})
+    private List<com.fk.framework.audit.Operator> employee;
+}
+
